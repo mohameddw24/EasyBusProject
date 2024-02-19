e023c47f00dd067ed8e73924b79c59d5427daf86
mohamedvw7 <mohamedvw7@gmail.com>
2024-02-19 21:33:16 +0200

    Remove User Image

---
+++ EasyBusProject/Migrations/20240219192957_Removeimg.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyBusProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class Removeimg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateTable(
+                name: "LoginUserVM",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    RememberMe = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LoginUserVM", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RegisterUserVM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ConfirmPassword = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RegisterUserVM", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LoginUserVM");
+
+            migrationBuilder.DropTable(
+                name: "RegisterUserVM");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Image",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+    }
+}
--- EasyBusProject/Models/User.cs
+++ EasyBusProject/Models/User.cs
@@ -6,7 +6,6 @@ namespace EasyBus.Models
     public class User : IdentityUser<int>
     {
         public string ClientName { get; set; } = string.Empty;
-        public byte[]? Image { get; set; }
         public virtual ICollection<UserSchedule>? UserSchedules { get; set; }
     }
 }
