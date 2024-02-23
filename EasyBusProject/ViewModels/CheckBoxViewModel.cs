b8a2c356b000d9608b10b929ee3989d4365a89a4
mohamedvw7 <mohamedvw7@gmail.com>
2024-02-23 13:53:26 +0200

    Seat Choosing

---
+++ EasyBusProject/ViewModels/CheckBoxViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EasyBusProject.ViewModels
+{
+    public class CheckBoxViewModel
+    {
+        public Dictionary<string, bool> CheckboxValues { get; set; }
+    }
+}
